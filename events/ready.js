42fb37a4bc5149dfded3d2dd6a9ec030ac3a76e5
Markus <meffesino@me.com>
2017-12-15 20:47:09 +0100

    minor changes.

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,6 @@ const log = message => {
 
 module.exports = client => { 
     client.user.setGame(`KD:M - White Secret Event`); 
-    //log(chalk.green(util.inspect(client))); //full text like 100 lines if you want to start this...
     log(chalk.bgGreen(`Bot online at Discord, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`)); 
-    
+
 };
